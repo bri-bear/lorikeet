f1d9252dbaa3b80b842b9e3e794645bf34183c73
bri-mintvm <briannastalkingdrum@gmail.com>
2022-08-23 00:04:58 -0300

    added init function

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
+#include <stdio.h>
+
 #include "config.h"
 #include <xcb/xcb.h>
+#include <xcb/xproto.h>
 
 xcb_generic_event_t *ev;
+xcb_screen_t *scr;
+xcb_window_t root;
 
-dpy = xcb_connect(NULL, NULL);
-if (xcb_connection_has_error(dpy)) {
-    printf("Error! Could not open display.");
-    return 1;
+xcb_connection_t *dpy; 
+int defaultScreen;
+
+void LorikeetTestWindow() {
+
+}
+
+void LorikeetInit() {
+    dpy = xcb_connect(NULL, NULL);
+    if (xcb_connection_has_error(dpy)) {
+        printf("Error! Could not open display.");
+    }
+
+
 }
 
 int main() {
